bec0018218467d5b7d052e816459ed4b317ee0e4
AtikaAbbasi <nonuabbasi855@gmail.com>
2025-04-17 18:27:09 +0500

    para changes

--- src/componets/footer.jsx
+++ src/componets/footer.jsx
@@ -9,19 +9,29 @@ const footer = () => {
   <div className= "logo">
   <span className= "yummy">Yummy</span><span className= "saga">Saga</span>
 </div>
-<p>This website is your go-to place for everything tasty! Discover mouth-watering recipes, explore the best foods, and enjoy a journey of flavors that delight your taste buds.</p>
+<p >This website is your go-to place for everything tasty! Discover mouth-watering recipes, explore the best foods, and enjoy a journey of flavors that delight your taste buds.
+From quick snacks to gourmet meals, we’ve got something delicious for every craving!</p>
     <hr />
    
   </div>
 
   <div className= "footer-bottom">
     <h6> © Copyright : Atika Aabasi</h6>
-    <div className= "social-icons">
+    {/* <div className= "social-icons">
       <a href="#"><i className= "fab fa-facebook-f"></i></a>
       <a href="#"><i className= "fab fa-instagram"></i></a>
       <a href="#"><i className= "fab fa-twitter"></i></a>
       <a href="#"><i className= "fab fa-youtube"></i></a>
-    </div>
+    </div> */}
+
+
+<div className="social-icons">
+  <a href="#"><i className="fab fa-facebook-f"></i></a>
+  <a href="#"><i className="fab fa-instagram"></i></a>
+  <a href="#"><i className="fab fa-twitter"></i></a>
+  <a href="#"><i className="fab fa-youtube"></i></a>
+</div>
+
   </div>
 </footer>
 
